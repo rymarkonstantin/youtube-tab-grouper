d0d6f9899a44286a940218507dabbf59413642da
Kanstantsin Rymar <kanstantsin_rymar@epam.com>
2025-12-08 15:15:42 +0300

    Centralize content script constants

---
+++ src/content/constants.js
@@ -0,0 +1,24 @@
+import { DEFAULT_SETTINGS } from '../shared/settings.js';
+
+export const CONTENT_SETTINGS_DEFAULTS = {
+    ...DEFAULT_SETTINGS
+};
+
+export const FALLBACK_GROUP = "Other";
+
+export const SELECTORS = {
+    title: "h1.title yt-formatted-string",
+    channelName: "ytd-channel-name a",
+    channelLink: "a.yt-simple-endpoint[href*='/channel/']",
+    channelHandleLink: "a.yt-simple-endpoint[href*='/@']",
+    descriptionMeta: "meta[name='description']",
+    keywordsMeta: "meta[name='keywords']",
+    jsonLdScript: 'script[type="application/ld+json"]',
+    genreMeta: "meta[itemprop='genre']"
+};
+
+export const BUTTON = {
+    id: "yt-grouper-btn",
+    label: "Group tab",
+    title: "Group this tab (Ctrl+Shift+G)"
+};
