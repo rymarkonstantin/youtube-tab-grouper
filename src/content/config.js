c58243d5a6d0607b87ba5360cac8fdd14c4d46d4
Kanstantsin Rymar <kanstantsin_rymar@epam.com>
2025-12-08 15:43:06 +0300

    Add content messaging bridge helpers

--- src/content/config.js
+++ src/content/config.js
@@ -1,7 +1,6 @@
-import { MESSAGE_ACTIONS } from '../shared/messages.js';
-import { sendMessageSafe } from '../shared/messaging.js';
 import { withSettingsDefaults } from '../shared/settings.js';
 import { CONTENT_SETTINGS_DEFAULTS } from './constants.js';
+import { sendGetSettings } from './messaging.js';
 
 export const normalizeContentSettings = (settings = {}) =>
     withSettingsDefaults({ ...CONTENT_SETTINGS_DEFAULTS, ...(settings || {}) });
@@ -10,7 +9,7 @@ export const isEnabled = (settings) => settings?.extensionEnabled !== false;
 
 export async function loadConfig() {
     try {
-        const response = await sendMessageSafe(MESSAGE_ACTIONS.GET_SETTINGS, {});
+        const response = await sendGetSettings();
         if (response?.success && response.settings) {
             return normalizeContentSettings(response.settings);
         }
